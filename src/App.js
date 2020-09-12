69317927da547e8ba6eb5c6e7a842c24878f919a
Craven <zwthomas13@gmail.com>
2020-09-11 23:17:56 -0500

    working on adding dark theme toggle

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
+import Switch from '@material-ui/core/Switch';
+import Slider from "./components/slider/Slider"
 import axios from 'axios';
 import clear_day from "./imgs/clear_day.svg"
 import clear_night from "./imgs/clear_night.svg"
@@ -31,7 +33,16 @@ import snow from "./imgs/snow.svg"
 import snow_heavy from "./imgs/snow_heavy.svg"
 import snow_light from "./imgs/snow_light.svg"
 import tstorm from "./imgs/tstorm.svg"
-import moon from "./imgs/moon1.svg"
+import first_quarter from "./imgs/first_quarter.svg"
+import  full from "./imgs/full.svg"
+import  last_quarter from "./imgs/last_quarter.svg"
+import  new_moon from "./imgs/new.svg"
+import  waning_crescent from "./imgs/waning_crescent.svg"
+import  waning_gibbous from "./imgs/waning_gibbous.svg"
+import  waxing_crescent from "./imgs/waxing_crescent.svg"
+import waxing_gibbous from "./imgs/waxing_gibbous.svg"
+import citation from "./imgs/citation.svg"
+
 // https://www.climacell.co/
 
 
@@ -44,20 +55,112 @@ async function getWeather(event, setWeather) {
 
   const response = await axios.get("https://api.climacell.co/v3/weather/forecast/daily?lat=41.881832&lon=-87.623177&unit_system=us&start_time=now&fields=precipitation%2Csunrise%2Csunset%2Cprecipitation_probability%2Cmoon_phase%2Cweather_code%2Ctemp&apikey=")
   const current = await axios.get("https://api.climacell.co/v3/weather/realtime?lat=41.881832&lon=-87.623177&unit_system=us&fields=temp%2Cweather_code%2Cmoon_phase%2Csunrise%2Csunset&apikey=")
-  // alert(JSON.stringify(current.data.temp.value))
+  alert(JSON.stringify(current.data))
   // alert(current.data.temp.value)
   setWeather({
     city: "Chicago, IL", 
     temp: current.data.temp.value,
     weather_code: current.data.weather_code.value,
-    moon_phase: current.data.moon_phase.value
+    moon_phase: current.data.moon_phase.value,
+    sunrise: new Date(current.data.sunrise.value).toLocaleTimeString(),
+    sunset: new Date(current.data.sunset.value).toLocaleTimeString()
   })
+}
+
+function nameForDisplay(codeName) {
+  let displayName = []
+  codeName.split("_").forEach(part => 
+    displayName.push(part.charAt(0).toUpperCase() + part.slice(1))
+  )
+  return displayName.join(" ")
+}
+
+function selectMoonIcon(codeName) {
+  switch(codeName) {
+    case "first_quarter":
+      return first_quarter;
+    case "full":
+      return full
+    case "last_quarter":
+      return last_quarter
+    case "new":
+      return new_moon
+    case "waning_crescent":
+      return waning_crescent
+    case "waning_gibbous":
+      return waning_gibbous
+    case "waxing_gibbous":
+      return waxing_gibbous
+    case "waxing_crescent":
+    return waxing_crescent
+  }
+}
+
+function selectWeatherIcon(codeName) {
+
+  switch(codeName) {
+    case "freezing_rain_heavy":
+      return freezing_rain_heavy;
+    case "freezing_rain":
+      return freezing_rain;
+    case "freezing_rain_light":
+      return freezing_rain_light;
+    case "freezing_drizzle":
+      return freezing_drizzle;
+    case "ice_pellets_heavy":
+      return ice_pellets_heavy;
+    case "ice_pellets":
+      return ice_pellets;
+    case "ice_pellets_light":
+      return ice_pellets_light;
+    case "snow_heavy":
+      return snow_heavy;
+    case "snow":
+      return snow;
+    case "snow_light":
+      return snow_light;
+    case "flurries":
+      return flurries;
+    case "tstorm":
+      return tstorm;
+    case "rain_heavy":
+      return rain_heavy;
+    case "rain":
+      return rain;
+    case "rain_light":
+      return rain_light;
+    case "drizzle":
+      return drizzle;
+    case "fog_light":
+      return fog_light;
+    case "fog":
+      return fog;
+    case "cloudy":
+      return cloudy;
+    case "mostly_cloudy":
+      return mostly_cloudy;
+    case "partly_cloudy":
+      return partly_cloudy_day;
+    case "partly_cloudy":
+      return ;
+    case "mostly_clear":
+      return mostly_clear_day;
+    case "mostly_clear":
+      return ;
+    case "clear":
+      return clear_day;
+    case "clear":
+      return ;
+  }
+
 }
 
 function App() {
   const weatherLookup = {
     "clear_day":"",
     "clear_night":"",
+
+
     "cloudy":"",
     "drizzle":"",
     "flurries":"",
@@ -70,6 +173,7 @@ function App() {
     "ice_pellets":"",
     "ice_pellets_heavy":"",
     "ice_pellets_light":"",
+    
     "mostly_clear_day":"",
     "mostly_clear":["Mostly Clear", mostly_clear_night],
     "mostly_cloudy":"",
@@ -85,11 +189,21 @@ function App() {
     "---":""
   
   }
-
-  const [weather, setWeather] = useState({city: "----",temp: "--", weather_code: "---", moon_phase:"---"});
+//weatherLookup[weather.weather_code][1]
+  const [weather, setWeather] = useState({
+    city: "----",
+    temp: "--", 
+    weather_code: "---", 
+    moon_phase:"---",
+    sunrise: "-",
+    sunset: "-"
+  
+  });
   return (
-    <div className="App">
-      <div className="holder">
+    <div className="App" >
+      
+      <div className="holder" >
+      <Switch className="bob"/>
         <Form onSubmit={(event) => getWeather(event,setWeather)}>
           <div className="search">
             <Form.Control type="textarea" placeholder="🔍 Search" />
@@ -98,19 +212,24 @@ function App() {
           <div className="weather">
             <h1>➤ {weather.city}</h1>
             <div className="currentConditions">
-              <img src={weatherLookup[weather.weather_code][1]} width="64" height="64" /> 
+              <img src={selectWeatherIcon(weather.weather_code)} width="64" height="64" /> 
               <h2 className="temp">{weather.temp}°</h2> 
-              <h3 className="condition">{weatherLookup[weather.weather_code][0]}</h3>
+              <h3 className="condition">{nameForDisplay(weather.weather_code)}</h3>
+            </div>
+            <div className="sun">
+              <h3><b>Rise:</b> {weather.sunrise} <b>Set:</b> {weather.sunset}</h3>
             </div>
             <div className="moon"> 
-            <img src={moon} width="64" height="64" /> 
-            <h3>{weather.moon_phase}</h3>
-
+              <img src={selectMoonIcon(weather.moon_phase)} width="64" height="64" /> 
+              <h3 className="moonPhase">{nameForDisplay(weather.moon_phase)}</h3>
             </div>
           </div>
         </Form>
       </div>
+      
+      <img className="citation" src={citation} />
     </div>
+    
   );
 }
 
