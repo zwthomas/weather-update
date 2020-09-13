0198ec7f59d42d73034e1f0fb048a4d014fb3ac2
Craven <zwthomas13@gmail.com>
2020-09-12 19:24:01 -0500

    Updates

---
+++ precache-manifest.f0f962b8211791fdfa29e75537a16aa8.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70e8ff6a3497a980d3a14bc547cade8a",
+    "url": "/weather-update/index.html"
+  },
+  {
+    "revision": "887a4c6cc5c2fb9d20d5",
+    "url": "/weather-update/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "c4e8a631713bf72a5100",
+    "url": "/weather-update/static/css/main.505e7d3d.chunk.css"
+  },
+  {
+    "revision": "887a4c6cc5c2fb9d20d5",
+    "url": "/weather-update/static/js/2.f81258d2.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/weather-update/static/js/2.f81258d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4e8a631713bf72a5100",
+    "url": "/weather-update/static/js/main.f33204dc.chunk.js"
+  },
+  {
+    "revision": "19bb503e81fe277a1e14",
+    "url": "/weather-update/static/js/runtime-main.cc869ec5.js"
+  },
+  {
+    "revision": "27c8fdc0ca2516893433d1f25bdfa51b",
+    "url": "/weather-update/static/media/citation.27c8fdc0.svg"
+  },
+  {
+    "revision": "9d31decab753db97fbd090d1bdad6299",
+    "url": "/weather-update/static/media/clear_day.9d31deca.svg"
+  },
+  {
+    "revision": "ef473ad9150ddacc2122e862530bbcce",
+    "url": "/weather-update/static/media/clear_night.ef473ad9.svg"
+  },
+  {
+    "revision": "e4c1f2cfbb977f646dafa668db00dcc1",
+    "url": "/weather-update/static/media/cloudy.e4c1f2cf.svg"
+  },
+  {
+    "revision": "1ee49c93f335788b8bf7c5137edb3de8",
+    "url": "/weather-update/static/media/drizzle.1ee49c93.svg"
+  },
+  {
+    "revision": "4abb1006d24b367b3065636770f21232",
+    "url": "/weather-update/static/media/first_quarter.4abb1006.svg"
+  },
+  {
+    "revision": "9bedcf866a21b7d1390069422774758d",
+    "url": "/weather-update/static/media/flurries.9bedcf86.svg"
+  },
+  {
+    "revision": "44096d7e0578cb8213ecc5f1e9d23602",
+    "url": "/weather-update/static/media/fog.44096d7e.svg"
+  },
+  {
+    "revision": "997c49a09f2c0f32afd4e72da95ceb58",
+    "url": "/weather-update/static/media/fog_light.997c49a0.svg"
+  },
+  {
+    "revision": "a5c6c7759fc7ddc25156b346256b905b",
+    "url": "/weather-update/static/media/freezing_drizzle.a5c6c775.svg"
+  },
+  {
+    "revision": "f57ff72c2411ce9e06c765c07459fdc1",
+    "url": "/weather-update/static/media/freezing_rain.f57ff72c.svg"
+  },
+  {
+    "revision": "50f3e5e7bdb64eee9a447fa6399b2a7f",
+    "url": "/weather-update/static/media/freezing_rain_heavy.50f3e5e7.svg"
+  },
+  {
+    "revision": "4827759bda4f7f32c37bdfe2839fd5c4",
+    "url": "/weather-update/static/media/freezing_rain_light.4827759b.svg"
+  },
+  {
+    "revision": "5c96ec4655c20a88b1aa02fc3b9b7d56",
+    "url": "/weather-update/static/media/full.5c96ec46.svg"
+  },
+  {
+    "revision": "f090e78938f5c318b6fd38cfaa343716",
+    "url": "/weather-update/static/media/ice_pellets.f090e789.svg"
+  },
+  {
+    "revision": "5ee0972c564f792d71f4252296ce9cb0",
+    "url": "/weather-update/static/media/ice_pellets_heavy.5ee0972c.svg"
+  },
+  {
+    "revision": "54609c36c4e0842b48b93e1079209f7f",
+    "url": "/weather-update/static/media/ice_pellets_light.54609c36.svg"
+  },
+  {
+    "revision": "5ba3f07b6b295015a2fcc93aad92e816",
+    "url": "/weather-update/static/media/last_quarter.5ba3f07b.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-update/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "689ef5f04206500442832c23430f83c9",
+    "url": "/weather-update/static/media/mostly_clear_day.689ef5f0.svg"
+  },
+  {
+    "revision": "5b6b66126747bdbd7aeacfae7e276fc5",
+    "url": "/weather-update/static/media/mostly_clear_night.5b6b6612.svg"
+  },
+  {
+    "revision": "3784af722503735542b3007cab5207f4",
+    "url": "/weather-update/static/media/mostly_cloudy.3784af72.svg"
+  },
+  {
+    "revision": "06cdfc9649658312be4aa33c1f4e0b19",
+    "url": "/weather-update/static/media/new.06cdfc96.svg"
+  },
+  {
+    "revision": "5c211d88d39a7914db7ce24d2d6962db",
+    "url": "/weather-update/static/media/partly_cloudy_day.5c211d88.svg"
+  },
+  {
+    "revision": "54fd940004a5e2be66a4c8b92bc5bfac",
+    "url": "/weather-update/static/media/partly_cloudy_night.54fd9400.svg"
+  },
+  {
+    "revision": "e4374187184e33c961540e5fb9544135",
+    "url": "/weather-update/static/media/precip.e4374187.svg"
+  },
+  {
+    "revision": "e3eb4f73f9f17dcbfeb39efca169bea9",
+    "url": "/weather-update/static/media/rain.e3eb4f73.svg"
+  },
+  {
+    "revision": "bd48faa0b80d6a4d9c2689c039bc2c50",
+    "url": "/weather-update/static/media/rain_heavy.bd48faa0.svg"
+  },
+  {
+    "revision": "26a314dee61cea166c95de5bcf95a241",
+    "url": "/weather-update/static/media/rain_light.26a314de.svg"
+  },
+  {
+    "revision": "5a4d5c569b044a9a8af45c887316b1aa",
+    "url": "/weather-update/static/media/snow.5a4d5c56.svg"
+  },
+  {
+    "revision": "4ec83184553f0aa4488431d2051dbf7e",
+    "url": "/weather-update/static/media/snow_heavy.4ec83184.svg"
+  },
+  {
+    "revision": "09e6c94d194a6dd881dbaba82c6b44c4",
+    "url": "/weather-update/static/media/snow_light.09e6c94d.svg"
+  },
+  {
+    "revision": "53d2b84369818ff58bb573067af7eb12",
+    "url": "/weather-update/static/media/tstorm.53d2b843.svg"
+  },
+  {
+    "revision": "4c40b2df11c63d7eab56f1aa5e097b66",
+    "url": "/weather-update/static/media/waning_crescent.4c40b2df.svg"
+  },
+  {
+    "revision": "a84aae000ae458e4e080aaf73e5e6fed",
+    "url": "/weather-update/static/media/waning_gibbous.a84aae00.svg"
+  },
+  {
+    "revision": "407d8e6177b02e272ea0857d09002b53",
+    "url": "/weather-update/static/media/waxing_crescent.407d8e61.svg"
+  },
+  {
+    "revision": "3a58b2c4f83ed87ac97879a499082ab5",
+    "url": "/weather-update/static/media/waxing_gibbous.3a58b2c4.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/weather-update/precache-manifest.f0f962b8211791fdfa29e75537a16aa8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-update/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
